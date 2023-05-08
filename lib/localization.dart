695a8e4373da48396c6b6c4479ccd5bda33d5f17
naturalitchy <nsubway635@gmail.com>
2023-05-08 22:13:07 +0900

    only first screen change lang.

--- lib/localization.dart
+++ lib/localization.dart
@@ -1,19 +1,60 @@
 
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-class Localization extends StatelessWidget {
+class Localization extends StatefulWidget {
   const Localization({Key? key}) : super(key: key);
+
+  @override
+  State<Localization> createState() => _LocalizationState();
+}
+
+class _LocalizationState extends State<Localization> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    super.initState();
+  }
 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        // color: Colors.black,
-        // child: Center(
-        //   child: Image(
-        //     image: AssetImage('assets/images/uzko_logo_ai_ex1-removebg-preview.png'),
-          // ),
-        // ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 100.0),
+            child: Row(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    context.setLocale(Locale('ko', 'KO'));
+                    print('execute ko button');
+                  },
+                  child: Text('Translate to Korean'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    context.setLocale(Locale('en', 'US'));
+                  },
+                  child: Text('Translate to English'),
+                ),
+
+              ],
+            ),
+          ),
+
+          SizedBox(height: 130,),
+
+          Container(
+            child: Center(
+              child: Text('one').tr(),
+            ),
+          ),
+        ],
       ),
+
+
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,16 @@ class MyApp extends StatelessWidget {
  */
 
 
+/**
+ *
+ * 이거 처음에 나오는 기준이 뭐냐.
+ * 어떻게 해서 es-US부터 나오는지 찾아라.
+ * 그리고, 클래스 여러개 만들어서 전체 테스트 해라. 버튼은 appBar를 만들어서 어디든 끌고다니고.
+ * 버튼을 눌렀을때 하나의 화면만 변하는게 아니라 다른 화면도 변해야 한다.
+ * 그리고 다시 다른화면으로 이동하였을때 상태를 그대로 유지하고 있어야 한다.
+ */
+
+
 
 
 
@@ -47,24 +57,38 @@ Future<void> main() async {
 
   runApp(
     EasyLocalization(
-        supportedLocales: [Locale('en'), Locale('ko')],
-        path: 'assets/translations/', // <-- change the path of the translation files
-        // fallbackLocale: Locale('en', 'US'),
+        supportedLocales: [Locale('en', 'US'), Locale('ko', 'KO')],
+        path: 'assets/translations', // <-- change the path of the translation files
+        fallbackLocale: Locale('ko', 'KO'),
         child: MyApp(),
+
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // context.setLocale(Locale('en', 'US'));
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
         localizationsDelegates: context.localizationDelegates,
         supportedLocales: context.supportedLocales,
         locale: context.locale,
-        // home: Localization(),
+        home: Localization(),
     );
   }
 }
